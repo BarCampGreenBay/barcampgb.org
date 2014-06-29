03991f53d8a15ed28b14ad5d0735b97e6d5ec635
Chris Jaure <chrisjaure@gmail.com>
2014-06-29 15:30:20 -0500

    add dev mode

--- src/web/app.js
+++ src/web/app.js
@@ -1,4 +1,13 @@
+var nunjucks = require('nunjucks');
 var express = require('express');
 var app = express();
 
 module.exports = app;
+
+nunjucks.configure(__dirname + '/views', {
+	express: app
+});
+
+app.route('/').get(function(req, res) {
+	res.render('index.html');
+});
