5c40a03ea9a3db86f3e75cb0945c80960d03bb72
Chris Jaure <chrisjaure@gmail.com>
2014-06-28 22:57:58 -0500

    add nginx role and proxy config

--- src/api/server.js
+++ src/api/server.js
@@ -2,4 +2,9 @@ var http = require('http');
 var app = require('./app');
 var server = http.createServer(app);
 
-server.listen(process.env.API_PORT || 8000);
+server.listen(process.env.NODE_PORT, function(err) {
+	if (err) {
+		throw new Error(err);
+	}
+	console.log("BCGB Api server listening on %s", process.env.NODE_PORT);
+});
--- src/web/server.js
+++ src/web/server.js
@@ -2,4 +2,9 @@ var http = require('http');
 var app = require('./app');
 var server = http.createServer(app);
 
-server.listen(process.env.WEB_PORT || 8002);
+server.listen(process.env.NODE_PORT, function(err) {
+	if (err) {
+		throw new Error(err);
+	}
+	console.log("BCGB Web server listening on %s", process.env.NODE_PORT);
+});
