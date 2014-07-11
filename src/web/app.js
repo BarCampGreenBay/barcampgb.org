0c41d1cde4d4cb42d684960a97a8182ae4fa8ab5
Chris Jaure <chrisjaure@gmail.com>
2014-07-11 10:33:23 -0500

    get polymer working

--- src/web/app.js
+++ src/web/app.js
@@ -8,6 +8,10 @@ nunjucks.configure(__dirname, {
 	express: app
 });
 
+app.locals.vendor_components = require('./modules/vendor_components');
+
 app.route('/').get(function(req, res) {
 	res.render('views/index.html');
 });
+
+app.use('/vendor', express.static(__dirname + '/../../bower_components'));
---
+++ src/web/modules/vendor_components.js
@@ -0,0 +1,4 @@
+module.exports = [
+	'core-icons',
+	'core-icon'
+];
