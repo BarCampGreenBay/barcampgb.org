5b16f4da36ceb47b5033ae58ddd089ddbda99461
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-07-31 12:41:58 -0500

    Clean up intro jumbotron on mobile, other details

--- src/web/assets/js/client.jsx
+++ src/web/assets/js/client.jsx
@@ -36,22 +36,57 @@ fullScreenVideo = {
     var height = $( window ).height();
     textWidth = $("#text").width();
     textHeight = $("#text").height();
-    $("#video_container").css("width", width).css("height", (height * .75));
+    if (width <= 768) {
+      $("#video_container").css("width", width).css("height", (height));
+      $("#text").css("left", (width/2) - (textWidth/1.95)).css("top", (height/2) - (textHeight/2.5));
+    } else {
+      $("#video_container").css("width", width).css("height", (height * .75));
+      $("#text").css("left", (width/2) - (textWidth/1.97)).css("top", (height/3) - (textHeight/2.5));
+    }
     $("#main-image").css("width", width).css("height", height);
-    $("#text").css("left", (width/2) - (textWidth/2)).css("top", (height/3) - (textHeight/2.5));
   }
 
 }
 
 var main = function() {
+  var scroll = $(window).scrollTop();
+  var scrollHeight = $(".jumbotron").height();
+  var showBackground = function() {
+    $(".navbar-background").css("opacity", "1");
+  }
+  var hideBackground = function() {
+    $(".navbar-background").css("opacity", "0");
+  }
+  var open;
+  $('#navbar-collapse-1').on('show.bs.collapse', function () {
+    showBackground();
+    return open = true;
+  });
+  $('#navbar-collapse-1').on('hidden.bs.collapse', function () {
+    hideBackground();
+    return open = false;
+  });
+  console.log(open);
+  // If the scroll goes past the jumbotron's height, show the background, otherwise hide it
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    if (scroll >= 457) {
-      $(".navbar-background").css("opacity", "1");
-    } else {
-      $(".navbar-background").css("opacity", "0");
+    console.log(open);
+    if (scroll >= (scrollHeight - 100)) {
+      showBackground();
+      $('#navbar-collapse-1').on('hidden.bs.collapse', function () {
+        showBackground();
+        return open = false;
+      });
+    } else if ((scroll < (scrollHeight - 100)) && open == false) {
+      hideBackground();
+      $('#navbar-collapse-1').on('hidden.bs.collapse', function () {
+        hideBackground();
+        return open = false;
+      });
     }
   });
+
+  // If the scroll is between the top and the bottom of the jumbotron and the menu is opened, show the background.
 }
 
 $(document).ready(main);
